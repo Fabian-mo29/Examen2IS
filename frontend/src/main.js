f2936f5c2e2d7f70f520f0721c16f90dc12077d4
Fabian <fabian.moraaria@ucr.ac.cr>
2025-07-12 22:12:40 -0600

    Frontend para la lista de sodas

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import api from "./Api/api";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(api);
+app.mount("#app");
